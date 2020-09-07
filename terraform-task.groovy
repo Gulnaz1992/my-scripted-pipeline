cb9a089202d34f998a7fc35b125102aeacf56e0f
Gulnaz1992 <gulnaz.kgs@gmail.com>
2020-09-07 21:02:19 +0000

    added terraform-task final

--- terraform-task.groovy
+++ terraform-task.groovy
@@ -1,14 +1,20 @@
-node {
+node{
     stage("Pull Repo"){
-        ssh 'echo "{Pulling Repo"'
+        git branch: 'solution', changelog: false, poll: false, url: 'https://github.com/ikambarov/terraform-task.git'
     }
-
-    stage ("Pull Repo"){
-        ssh 'echo "Terraform Init"'
-    }
-
-    stage("Terraform Apply"){
-        ssh 'echo "Terraform Apply"'
-    }
-
+    withCredentials([usernamePassword(credentialsId: 'jenkins-aws-access-key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
+        stage("Terrraform Init"){
+            sh '''
+                cd sandbox/
+                terraform-0.13 version
+                terraform-0.13 init
+            '''
+        }
+        stage("Terraform Apply"){
+            sh '''
+                cd sandbox/
+                terraform-0.13 apply -auto-approve
+            '''
+        }
+    }    
 }
